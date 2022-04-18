50dde1cfecc0eaf02d2e2ae1188603656cc28cd1
NNBB123456 <96527759+NNBB123456@users.noreply.github.com>
2022-04-18 20:25:05 +0530

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -1,22 +1,37 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var color = "red";
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=2;
-ctx.arc(200,200,40,0,2*Math.PI);
-ctx.stroke();
-canvas.addEventListener("mousedown",my_mousedown);
-function my_mousedown(e){
-    mouse_x=e.clientX -canvas.offsetLeft;
-    mouse_y=e.clientY -canvas.offsetTop;
-    console.log("X="+ mouse_x+"Y="+ mouse_y);
-    circle(mouse_x,mouse_y)
+var mouse_event = "empty";
+var last_position_of_x,last_position_of_y;
+var width = 1;
+canvas.addEventListener("mousedown",my_mouse_down);
+function my_mouse_down(e){
+    mouse_event = "mouseDown";
 }
-function circle(mouse_x,mouse_y){
-    ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=2;
-ctx.arc(mouse_x,mouse_y,40,0,2*Math.PI);
-ctx.stroke();
+canvas.addEventListener("mouseup",my_mouse_up);
+function my_mouse_up(e){
+    mouse_event = "mouseup";
+}
+canvas.addEventListener("mouseleave",my_mouse_leave);
+function my_mouse_leave(e){
+    mouse_event = "mouseleave";
+}
+canvas.addEventListener("mousemove",my_mouse_move);
+function my_mouse_move(e){
+    var current_position_of_x=e.clientx-canvas.offsetLeft;
+    var current_position_of_y=e.clienty-canvas.offsetTop;
+    if (mouse_event=="mouseDown"){
+        ctx.beginPath();
+        ctx.strokeStyle=color;
+        ctx.lineWidth;
+        console.log("last_position_of_x_and_y=");
+        console.log("x="+last_position_of_x+"y="+last_position_of_y);
+        ctx.moveTo(last_position_of_x,last_position_of_y);
+        console.log("current_position_of_x_and_y=");
+        console.log("x="+current_position_of_x+"y="+current_position_of_y);
+        ctx.lineTo(current_position_of_x,current_position_of_y);
+        ctx.stroke();
+    }
+    last_position_of_x=current_position_of_x;
+    last_position_of_y=current_position_of_y;
 }
